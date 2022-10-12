cfaa1cf7ba1b5a6f29254a8569088801c12f600d
Tommy Han <tommyhonym@gmail.com>
2022-10-12 15:49:16 +0800

    Revert beack to use iOS14
    SimplePing dependency requires to have iOS14 as the minimum version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SimpleTracer",
-    platforms: [.macOS(.v11), .iOS(.v13)],
+    platforms: [.macOS(.v11), .iOS(.v14)],
     products: [
         .library(
             name: "SimpleTracer",
